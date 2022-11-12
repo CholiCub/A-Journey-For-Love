e74965f82e168df2c3bda7371078f7d8bfed8ea7
Karen Flores <cholicub@gmail.com>
2022-11-12 00:25:28 -0500

    Refactored question options Now prints with same template function

--- AJourenyForLove-Text-BasedAdventureGame/Main.cpp
+++ AJourenyForLove-Text-BasedAdventureGame/Main.cpp
@@ -10,36 +10,27 @@
 #include "Player.h"
 using namespace std;
 
-//protoypes
 void ToUpper(string& oldString);
-
-//player visuals
 void GetPlayerJournalSetting();
-void PrintIdentityOptions();
-void PrintRaceOptions();
-void PrintBodyTypeOptions();
-void PrintReligionOptions(); 
-void PrintEyeColorOptions();
-void PrintHairColorOptions();
-void PrintPartnerPreferenceOptions();
-bool IsValidIntInEnum(const int NUM_OF_ITEMS, int playerInput);
-
-string name{};
-Identity identity{};
-Race race{};
-BodyType bodyType{};
-Religion religion{};
-HairColor hairColor{};
-EyeColor eyeColor{};
-PartnerPreference partnerPreference{};
+bool IsValidIntInEnum(const int numOfItems, int playerInput);
+template <class myType> void PrintQuestionOptions(const int numOfOptions, string(*s)(myType));
 
 int main() {
+
+	Identity identity{};
+	Race race{};
+	BodyType bodyType{};
+	Religion religion{};
+	HairColor hairColor{};
+	EyeColor eyeColor{};
+	PartnerPreference partnerPreference{};
+
 	int age{};
 	char dash{};
-	int heightInInches{}, bodyType{};
+	int heightInInches{}, bodyTypeNum{};
 	int likeAnimals{};
-
 	bool identityValid{};
+
 	GetPlayerJournalSetting();
 }
 
@@ -52,17 +43,9 @@ void ToUpper(string& oldString) {
 
 void GetPlayerJournalSetting() {
 	bool playerInputValid{ };
+	string name{};
 	int age{};
-	int identityNum{}, raceNum{}, bodyTypeNum{}, religionNum{}, hairColorNum{}, eyeColorNum{},partnerPreferenceNum{};
-	std::cout<<"Welcome to A Journey for Love";
-	std::cout<<"\nEnter first name: ";
-	getline(std::cin, name);
-
-	do {
-		std::cout << "Enter age: ";
-		cin >> age;
-	} while (!(age >= 18 && age<105));
-
+	int identityNum{}, raceNum{}, bodyTypeNum{}, religionNum{}, hairColorNum{}, eyeColorNum{}, partnerPreferenceNum{};
 	bool IsIdenityValid{};
 	bool IsRaceValid{};
 	bool IsBodyTypeValid{};
@@ -70,9 +53,19 @@ void GetPlayerJournalSetting() {
 	bool IsEyeColorValid{};
 	bool IsHairColorValid{};
 	bool IsPartnerPreferenceValid{};
+
+	std::cout << "Welcome to A Journey for Love";
+	std::cout << "\nEnter first name: ";
+	getline(std::cin, name);
+
+	do {
+		std::cout << "Enter age: ";
+		cin >> age;
+	} while (!(age >= 18 && age < 105));
+
 	do {
 		std::cout << "Enter the identity you best identify with: " << endl;
-		PrintIdentityOptions();
+		PrintQuestionOptions(NUM_OF_IDENTITIES, IdentityToString);
 		std::cin >> identityNum;
 		IsIdenityValid = IsValidIntInEnum(NUM_OF_IDENTITIES, identityNum);
 		if (!IsIdenityValid) {
@@ -81,7 +74,7 @@ void GetPlayerJournalSetting() {
 	} while (!IsIdenityValid);
 	do {
 		std::cout << "Enter the race you best identify with: " << endl;
-		PrintRaceOptions();
+		PrintQuestionOptions(NUM_OF_RACES, RaceToString);
 		std::cin >> raceNum;
 		IsRaceValid = IsValidIntInEnum(NUM_OF_RACES, raceNum);
 		if (!IsRaceValid) {
@@ -90,8 +83,7 @@ void GetPlayerJournalSetting() {
 	} while (!IsRaceValid);
 	do {
 		std::cout << "Enter the body type that best describes you: " << endl;
-		PrintBodyTypeOptions();
-		
+		PrintQuestionOptions(NUM_OF_BODY_TYPES, BodyTypeToString);
 		std::cin >> bodyTypeNum;
 		IsBodyTypeValid = IsValidIntInEnum(NUM_OF_BODY_TYPES, bodyTypeNum);
 		if (!IsBodyTypeValid) {
@@ -99,90 +91,57 @@ void GetPlayerJournalSetting() {
 		}
 	} while (!IsBodyTypeValid);
 	do {
-		std::cout << "Enter the body type that best describes you: " << endl;
-		PrintReligionOptions();
-		
+		std::cout << "Enter the religion that best describes you: " << endl;
+		PrintQuestionOptions(NUM_OF_RELIGION, ReligionToString);
 		std::cin >> religionNum;
 		IsReligionValid = IsValidIntInEnum(NUM_OF_RELIGION, religionNum);
 		if (!IsBodyTypeValid) {
 			std::cout << "Invalid entry. Try again." << endl << endl;
 		}
 	} while (!IsReligionValid);
-	//PartnerPreference
 	do {
 		std::cout << "Enter your partner preference: " << endl;
-		PrintPartnerPreferenceOptions();
+		PrintQuestionOptions(NUM_OF_PARTNER_PREFERENCE, PartnerPreferenceToString);
 		std::cin >> partnerPreferenceNum;
 		IsPartnerPreferenceValid = IsValidIntInEnum(NUM_OF_PARTNER_PREFERENCE, partnerPreferenceNum);
 		if (!IsPartnerPreferenceValid) {
 			std::cout << "Invalid entry. Try again." << endl << endl;
 		}
 	} while (!IsPartnerPreferenceValid);
-		//EyeColor
 	do {
 		std::cout << "Enter your eye color: " << endl;
-		PrintEyeColorOptions();
+		PrintQuestionOptions(NUM_OF_EYE_COLORS, EyeColorToString);
 		std::cin >> eyeColorNum;
 		IsEyeColorValid = IsValidIntInEnum(NUM_OF_EYE_COLORS, eyeColorNum);
 		if (!IsEyeColorValid) {
 			std::cout << "Invalid entry. Try again." << endl << endl;
 		}
 	} while (!IsEyeColorValid);
-	//HairColor
 	do {
 		std::cout << "Enter your hair color: " << endl;
-		PrintHairColorOptions();
+		PrintQuestionOptions(NUM_OF_HAIR_COLORS, HairColorToString);
 		std::cin >> hairColorNum;
 		IsHairColorValid = IsValidIntInEnum(NUM_OF_HAIR_COLORS, hairColorNum);
 		if (!IsHairColorValid) {
 			std::cout << "Invalid entry. Try again." << endl << endl;
 		}
 	} while (!IsHairColorValid);
-	Player player(name,age, IdentityToString(Identity(identityNum)), 
-		RaceToString(Race(raceNum)),ReligionToString(Religion(religionNum)),BodyTypeToString(BodyType(bodyTypeNum)),
-		EyeColorToString(EyeColor(eyeColorNum)),HairColorToString(HairColor(hairColorNum)), 
+	Player player(name, age, IdentityToString(Identity(identityNum)),
+		RaceToString(Race(raceNum)), ReligionToString(Religion(religionNum)), BodyTypeToString(BodyType(bodyTypeNum)),
+		EyeColorToString(EyeColor(eyeColorNum)), HairColorToString(HairColor(hairColorNum)),
 		PartnerPreferenceToString(PartnerPreference(partnerPreferenceNum)));
 	player.PrintPlayerInfo();
-	std::cout << "Thank you for entering your settings!" << endl;
+	std::cout << "\nThank you for entering your settings!" << endl;
 }
 
-void PrintIdentityOptions() {
-	for (int i = 1; i <= NUM_OF_IDENTITIES; i++) {
-		std::cout << i << "." << IdentityToString(Identity(i)) << endl;
+template <class myType> void PrintQuestionOptions(const int numOfOptions,  string (*s)(myType)) {
+	for (int i = 1; i <= numOfOptions; i++) {
+		std::cout << i << "." << s(myType(i)) << endl;
 	}
 }
-void PrintRaceOptions() {
-	for (int i = 1; i <= NUM_OF_RACES; i++) {
-		std::cout << i << "." << RaceToString(Race(i)) << endl;
-	}
-}
-void PrintBodyTypeOptions() {
-	for (int i = 1; i <= NUM_OF_BODY_TYPES; i++) {
-		std::cout << i << "." << BodyTypeToString(BodyType(i)) << endl;
-	}
-}
-void PrintReligionOptions() {
-	for (int i = 1; i <= NUM_OF_RELIGION; i++) {
-		std::cout << i << "." << ReligionToString(Religion(i)) << endl;
-	}
-}
-void PrintPartnerPreferenceOptions() {
-	for (int i = 1; i <= NUM_OF_PARTNER_PREFERENCE; i++) {
-		std::cout << i << "." << PartnerPreferenceToString(PartnerPreference(i)) << endl;
-	}
-}
-void PrintEyeColorOptions() {
-	for (int i = 1; i <= NUM_OF_EYE_COLORS; i++) {
-		std::cout << i << "." << EyeColorToString(EyeColor(i)) << endl;
-	}
-}
-void PrintHairColorOptions() {
-	for (int i = 1; i <= NUM_OF_HAIR_COLORS; i++) {
-		std::cout << i << "." << HairColorToString(HairColor(i)) << endl;
-	}
-}
-bool IsValidIntInEnum(const int NUM_OF_ITEMS, int playerInput) {
-	if (playerInput > 0 && playerInput <= NUM_OF_ITEMS) {
+
+bool IsValidIntInEnum(const int numOfItems, int playerInput) {
+	if (playerInput > 0 && playerInput <= numOfItems) {
 		return true;
 	}
 	else return false;
