d78617b9d30e5835556b7def3bf95c59a0df73a2
Karen Flores <cholicub@gmail.com>
2023-03-16 23:26:43 -0400

    Fomat changes

--- AJourenyForLove-Text-BasedAdventureGame/Player.cpp
+++ AJourenyForLove-Text-BasedAdventureGame/Player.cpp
@@ -12,6 +12,7 @@ Player::Player() {
 	eyeColor = {};
 	partnerPreference = {};
 }
+
 Player::Player(string name, int age, string identity, string race, string religion, string bodyType, string hairColor, string eyeColor, string partnerPreference) {
 	this->name = name;
 	this->age = age;
@@ -23,6 +24,7 @@ Player::Player(string name, int age, string identity, string race, string religi
 	this->eyeColor = eyeColor;
 	this->partnerPreference = partnerPreference;
 }
+
 void Player::PrintPlayerInfo() {
 	std::cout << "Your Settings: " << endl;
 	std::cout << "---------------------------------"<< endl;
@@ -40,54 +42,71 @@ void Player::PrintPlayerInfo() {
 string Player::getName() const {
 	return this->name;
 }
+
 void Player::setName(const std::string& newName) {
 	this->name = newName;
 }
+
 int Player::getAge() const{
 	return this->age;
 }
+
 void Player::setAge(int newAge) {
 	this->age = newAge;
 }
+
 string Player::getIdentity() const {
 	return this->identity;
 }
+
 void Player::setIdentity(const string& newIdentity) {
 	this->identity = newIdentity;
 }
+
 string Player::getRace() const {
 	return this->race;
 }
+
 void Player::setRace(const string& newRace) {
 	this->race = newRace;
 }
+
 string Player::getReligion() const {
 	return this->religion;
 }
+
 void Player::setReligion(const string& newReligion) {
 	this->religion = newReligion;
 }
+
 string Player::getBodyType() const {
 	return this->bodyType;
 }
+
 void Player::setBodyType(const string& newBodyType) {
 	this->bodyType = newBodyType;
 }
+
 string Player::getHairColor() const {
 	return this->hairColor;
 }
+
 void Player::setHairColor(const string& newHairColor) {
 	this->hairColor = newHairColor;
 }
+
 string Player::getEyeColor() const {
 	return this->eyeColor;
 }
+
 void Player::setEyeColor(const string& newEyeColor) {
 	this->eyeColor = newEyeColor;
 }
+
 string Player::getPartnerPreference() const {
 	return this->partnerPreference;
 }
+
 void Player::setPartnerPreference(const string& newPartnerPreference) {
 	this->partnerPreference = newPartnerPreference;
 }
