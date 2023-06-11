6b6e004a124b22c881c928b97030df70b9591110
Karen Flores <cholicub@gmail.com>
2023-06-10 23:35:40 -0400

    Removed functions no longer need in Player class after json refactor

--- AJourenyForLove-Text-BasedAdventureGame/Player.cpp
+++ AJourenyForLove-Text-BasedAdventureGame/Player.cpp
@@ -1,18 +1,6 @@
 #include "Player.h"
 #include<iostream>
 
-Player::Player() {
-	name = {};
-	age = {};
-	identity = {};
-	race = {};
-	religion = {};
-	bodyType = {};
-	hairColor = {};
-	eyeColor = {};
-	partnerPreference = {};
-}
-
 Player::Player(string name, int age, EIdentity identity, ERace race, EReligion religion, EBodyType bodyType, EHairColor hairColor, EEyeColor eyeColor, EPartnerPreference partnerPreference) {
 	this->name = name;
 	this->age = age;
@@ -50,70 +38,34 @@ string Player::getName() const {
 	return this->name;
 }
 
-void Player::setName(const std::string& newName) {
-	this->name = newName;
-}
-
 int Player::getAge() const{
 	return this->age;
-}
-
-void Player::setAge(int newAge) {
-	this->age = newAge;
 }
 
 EIdentity Player::getIdentity() const {
 	return this->identity;
 }
 
-void Player::setIdentity(const EIdentity& newIdentity) {
-	this->identity = newIdentity;
-}
-
 ERace Player::getRace() const {
 	return this->race;
-}
-
-void Player::setRace(const ERace& newRace) {
-	this->race = newRace;
 }
 
 EReligion Player::getReligion() const {
 	return this->religion;
 }
 
-void Player::setReligion(const EReligion& newReligion) {
-	this->religion = newReligion;
-}
-
 EBodyType Player::getBodyType() const {
 	return this->bodyType;
-}
-
-void Player::setBodyType(const EBodyType& newBodyType) {
-	this->bodyType = newBodyType;
 }
 
 EHairColor Player::getHairColor() const {
 	return this->hairColor;
 }
 
-void Player::setHairColor(const EHairColor& newHairColor) {
-	this->hairColor = newHairColor;
-}
-
 EEyeColor Player::getEyeColor() const {
 	return this->eyeColor;
 }
 
-void Player::setEyeColor(const EEyeColor& newEyeColor) {
-	this->eyeColor = newEyeColor;
-}
-
 EPartnerPreference Player::getPartnerPreference() const {
 	return this->partnerPreference;
-}
-
-void Player::setPartnerPreference(const EPartnerPreference& newPartnerPreference) {
-	this->partnerPreference = newPartnerPreference;
 }
--- AJourenyForLove-Text-BasedAdventureGame/Player.h
+++ AJourenyForLove-Text-BasedAdventureGame/Player.h
@@ -24,28 +24,17 @@ private:
 	EPartnerPreference partnerPreference;
 
 public:
-	Player();
 	Player(std::string name, int age, EIdentity identity, ERace race, EReligion religion, EBodyType bodyType, EHairColor hairColor, EEyeColor eyeColor, EPartnerPreference partnerPreference);
 	
 	void PrintPlayerInfo();
 
 	std::string getName() const;
-	void setName(const std::string& newName);
 	int getAge() const;
-	void setAge(int newAge);
 	EIdentity getIdentity() const;
-	void setIdentity(const EIdentity& newIdentity);
 	ERace getRace() const;
-	void setRace(const ERace& newRace);
 	EReligion getReligion() const;
-	void setReligion(const EReligion& newReligion);
 	EBodyType getBodyType() const;
-	void setBodyType(const EBodyType& newBodyType);
 	EHairColor getHairColor() const;
-	void setHairColor (const EHairColor& newHairColor);
 	EEyeColor getEyeColor() const;
-	void setEyeColor(const EEyeColor& newEyeColor);
 	EPartnerPreference getPartnerPreference() const;
-	void setPartnerPreference(const EPartnerPreference& newPartnerPreference);
-	
 };
