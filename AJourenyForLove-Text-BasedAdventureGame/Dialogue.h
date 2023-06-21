c8c1486f496e9f4117a14cf4eceb96df55b69652
Karen Flores <cholicub@gmail.com>
2023-06-20 23:08:42 -0400

    Added base properties to Dialogue class

--- AJourenyForLove-Text-BasedAdventureGame/Dialogue.h
+++ AJourenyForLove-Text-BasedAdventureGame/Dialogue.h
@@ -1,4 +1,25 @@
 #pragma once
+#include "Player.h"
+#include "Date.h"
+
+enum class EStage {
+	Greeting = 0,
+	Introduction,
+	Conversation,
+	Conculusion
+};
+
+enum class EConversationOptions{
+	Talk = 0,
+	Ask,
+	Listen,
+	Think
+};
+
 class Dialogue {
+	Player player;
+	Date date;
+public:
+	Dialogue(Player player, Date date) : player(player), date(date) {};
 };
 
