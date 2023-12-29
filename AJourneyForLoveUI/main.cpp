0e426115209a2c0da028a24bef06786a8b939f46
Karen Flores <cholicub@gmail.com>
2023-12-29 15:04:47 -0500

    Added basic main menu start button functionality

--- AJourneyForLoveUI/main.cpp
+++ AJourneyForLoveUI/main.cpp
@@ -6,18 +6,23 @@ int main() {
 	GameScreen gameScreen = TITLE;
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "A JOURNEY FOR LOVE");
 
+	Rectangle startBox = { SCREEN_WIDTH / 2 - 70,SCREEN_HEIGHT / 2 + 10, 225,50 };
+	bool mouseOnStartBox = false;
+	
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose()) {
+		//Update
+
+		Vector2 mousePos = GetMousePosition();
+		if ((CheckCollisionPointRec(mousePos, startBox))) { mouseOnStartBox = true; }
+		else mouseOnStartBox = false;
+		if (mouseOnStartBox && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) { /*gameScreen = PLAYER_SETTINGS;*/ DrawText("PLayer_settings scene", 20, 20, 20, RED);
+		}
 		BeginDrawing();
 		ClearBackground(WHITE);
-		DrawText("A JOURNEY FOR LOVE", SCREEN_WIDTH/2-100, SCREEN_HEIGHT/2-100, 20, SKYBLUE);
-		Rectangle startBox = { SCREEN_WIDTH / 2 - 100,SCREEN_HEIGHT / 2 - 50, 225,50 };
-		bool mouseOnText = false;
-		Vector2 mousePos = GetMousePosition();
-		if ((CheckCollisionPointRec(mousePos, startBox))) { mouseOnText = true; }
-		if (mouseOnText && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) { gameScreen = PLAYER_SETTINGS; }
-		else {}
+		DrawText("A JOURNEY FOR LOVE", SCREEN_WIDTH/2-130, SCREEN_HEIGHT/2-100, 25, SKYBLUE);
+		DrawText("START JOURNEY", SCREEN_WIDTH/2-70, SCREEN_HEIGHT/2+10, 20, mouseOnStartBox ? RED : BLACK);
 		EndDrawing();
 	}
 	CloseWindow();
